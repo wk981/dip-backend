655a6c743b17f55a3e02ad9aa234f443a5a8e7ae
wk981 <jamesim445@gmail.com>
2024-02-13 16:32:44 +0800

    Imported codes from dip chatbot. Created post route for chatbot and added invalid http request error handler

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # dip-backend
 Backend to be hosted on google functions
+
+# Packages
+pip install -U Flask
+pip install -U openai
+pip install -U instructor
+pip install -U python-dotenv 
+
+# How to run the development server
+python -m flask --app main.py run
--- main.py
+++ main.py
@@ -1,11 +1,113 @@
-from flask import Flask
+import os
+import json
+from flask import Flask, request, abort
+from werkzeug.exceptions import HTTPException
 
+# Import necessaries libs, taken from Omar repo
+from instructor import patch
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+# Initialize flask
 app = Flask(__name__)
+
+# Error handling, this will be invoked when the user tries to invoke a non existing route
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """Return JSON instead of HTML for HTTP errors."""
+    # start with the correct headers and status code from the error
+    response = e.get_response()
+    # replace the body with JSON
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return response
 
 # / Get route that returns hello world
 @app.route("/")
 def hello_world():
-    # Return JSON
+    # Return JSON and status 200
     return {
-        "message": "Hello World"
-    }   
+        "message": "Hello World",
+    }, 200
+
+# A POST route to /chatbot. It will take in a json req body and send it to openai api. Take in a json {message:}
+@app.route("/chatbot/send", methods=["POST"])
+def send_chatbot():
+    try:
+        # get value from request body. Only accept message
+        request_data = request.get_json()
+        # return bad request
+        if('message' not in request_data):
+            return {
+                    "message": "Bad Request"
+            }, 400
+        
+        else:
+            # print(request_data['message'])
+            # Credit to Omar
+            client = patch(OpenAI())
+
+            assistant = client.beta.assistants.retrieve(os.getenv('ASSISTANCE'))
+
+            thread = client.beta.threads.create()
+
+            message = client.beta.threads.messages.create(
+                thread_id = thread.id,
+                role="user",
+                content=request_data["message"]
+            )
+
+            run = client.beta.threads.runs.create(
+                thread_id=thread.id,
+                assistant_id=assistant.id,
+                instructions="You are a helpful assistant."
+            )
+
+            run_status = client.beta.threads.runs.retrieve(
+                        thread_id=thread.id,
+                        run_id=run.id
+            )
+
+            #Return "No message received, please try again" if there is not completed
+            if run_status.status != 'completed':
+                return {
+                    "message": "Something went wrong, please try again"
+                }, 503
+
+            else:
+                messages = client.beta.threads.messages.list(
+                    thread_id=thread.id
+                )
+                # print(messages)
+                # Response from api server
+                assistant_response = messages.data[0].content[0].text.value
+                if assistant_response == "":
+                    response_message = "No message received"
+                else:
+                    response_message = assistant_response
+                # return the relevant response code and message
+                return {
+                    "message": response_message
+                }, 204 if assistant_response == "" else None
+        
+    except Exception as e:
+        # If authentication error, return 401
+        print(e)
+        if(type(e).__name__ is "AuthenticationError"):
+            return {
+                "error":{
+                    "message": "Incorrect API key provided"
+                }
+            },401
+        # Otherwise return 500
+        else:
+            return {
+                "error":{
+                    "message": "Internal Server Error"
+                }
+            }, 500
